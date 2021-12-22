a73f47af8e28fc10bc5e59b3876d7aa9a0c7e90b
Victoria Da Luz <vicky.daluz@gmail.com>
2021-12-21 21:02:35 -0300

    uploading files

---
+++ src/components/ContentWrapper.jsx
@@ -0,0 +1,16 @@
+import TopBar from './TopBar';
+import ContentRowTop from './ContentRowTop';
+import Footer from './Footer';
+
+
+function ContentWrapper(){
+    return (
+        <div id="content-wrapper" className="d-flex flex-column">
+                <TopBar />
+                <ContentRowTop />
+                <Footer />
+        </div>    
+    )
+}
+
+export default ContentWrapper;
